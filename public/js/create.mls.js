71c3df8e92b0b377175363fb948b123c88759ef0
y0dev <devdoesit17@gmail.com>
2023-04-15 12:15:53 -0700

    tailwind has been added
     # * Created variable to store static database path
     # * UserID & document name is appended to database path variable to retrieve document

--- public/js/create.mls.js
+++ public/js/create.mls.js
@@ -38,16 +38,20 @@ function getCityAndState(str) {
 export const searchForMLS = async (mls_string) => {
   let url, data;
   console.log(mls_string);
+  const searchTitle = document.querySelector(".search-results .title");
+  const searchTerm = searchTitle.querySelector(".term");
   if (containsZipCode(mls_string)) {
     const zip_code = getZipCode(mls_string);
-    console.log(zip_code);
+    searchTerm.textContent = zip_code;
+    // console.log(zip_code);
     url = `http://localhost:${process.env.PORT}/api/v1/mls/searchZip`;
     data = {
       zip_code,
     };
   } else if (containsCityAndState(mls_string)) {
     const { city, state } = getCityAndState(mls_string);
-    console.log(city, state);
+    searchTerm.textContent = `${city}, ${state}`;
+    // console.log(city, state);
     url = `http://localhost:${process.env.PORT}/api/v1/mls/searchCS`;
     data = {
       city,
@@ -58,7 +62,11 @@ export const searchForMLS = async (mls_string) => {
     showAlert("fail", "Missing");
     return;
   }
+
+  const overlay = document.querySelector(".spinner-overlay");
   try {
+    // display loading overlay when making API call
+    overlay.style.display = "block";
     const res = await axios({
       method: "POST",
       url: url,
@@ -69,6 +77,7 @@ export const searchForMLS = async (mls_string) => {
       showAlert("success", "MLS finish successfully");
       const results = res.data.results;
       console.log(results);
+
       // Clear table
       const table = document.getElementById("home-table");
       const tbody = table.tBodies[0];
@@ -95,6 +104,14 @@ export const searchForMLS = async (mls_string) => {
         bedCell.textContent = item.beds;
         bathCell.textContent = item.baths;
       });
+
+      const tableDisplay = table.style.display;
+      if (tableDisplay === "none") {
+        searchTitle.style.display = "block";
+        tableDisplay = "block";
+      }
+      // hide loading overlay after updating table
+      overlay.style.display = "none";
 
       //   window.setTimeout(() => {
       //     location.assign("/");
