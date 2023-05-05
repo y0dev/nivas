a5a55fc2c456cfe38a153e84625b54ab86b388d6
Devontae Reid <devdoesit17@gmail.com>
2023-05-05 01:26:13 -0500

    add printouts to server.js

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ require("dotenv").config();
 
 process.on("uncaughtException", (err) => {
   logger.warn("UNHANDLED EXCEPTION! Shutting down...");
+  console.log("UNHANDLED EXCEPTION! Shutting down...");
   UtilityService.handleError(err);
   process.exit(1);
 });
 
 mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
   logger.info("Database connection successful");
+  console.log("Database connection successful");
 });
 
 const app = require("./app");
@@ -19,10 +21,12 @@ const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   logger.info(`localhost:${port}/`);
+  console.log("UNHANDLED REJECTION! Shutting down...");
 });
 
 process.on("unhandledRejection", (err) => {
   logger.warn("UNHANDLED REJECTION! Shutting down...");
+  console.log("UNHANDLED REJECTION! Shutting down...");
   UtilityService.handleError(err);
   server.close(() => {
     process.exit(1);
