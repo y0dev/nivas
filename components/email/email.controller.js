5bf4e2c02a230985f98b5edaa72a1494831abb26
y0dev <devdoesit17@gmail.com>
2024-07-05 23:23:27 -0500

    Title:Update Email controller

--- components/email/email.controller.js
+++ components/email/email.controller.js
@@ -3,10 +3,27 @@ const catchAsync = require("../../utils/catchAsync");
 const logger = require("../../utils/logger").logger;
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+/**
+ * Send a contact email
+ */
 exports.sendContactEmail = catchAsync(async (req, res, next) => {
-  req.params.id = req.user.id;
-  logger.info("Sending an contact email");
-  await new Email("", "").sendContactEmail();
+  try {
+    const { email, name, message } = req.body;
+    const user = { email, name }; // Assuming these fields are included in the request body
+    const url = ''; // Set URL as needed or use an appropriate value
 
-  next();
+    logger.info("Sending a contact email");
+    await new Email(user, url).sendContactEmail();
+
+    res.status(200).json({
+      status: "success",
+      message: "Contact email sent successfully",
+    });
+  } catch (error) {
+    logger.error("Failed to send contact email", error);
+    res.status(500).json({
+      status: "error",
+      message: "Failed to send contact email",
+    });
+  }
 });
